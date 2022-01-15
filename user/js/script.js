5d6d0255d09fa0c9ee5ac546951b5ca924c1552e
kgovind12 <kgovind@ucdavis.edu>
2022-01-14 17:31:07 -0800

    Changed variable name to username in script.js

--- user/js/script.js
+++ user/js/script.js
@@ -1,6 +1,6 @@
 // display first name on main app page
 let params = (new URL(document.location)).searchParams;
-let name = params.get("userName");
-let first_name = name.split(" ")[0];
-console.log("User Name Received: "+name);
+let username = params.get("userName");
+let first_name = username.split(" ")[0];
+console.log("User Name Received: " + username);
 document.getElementById("username").textContent = first_name;
